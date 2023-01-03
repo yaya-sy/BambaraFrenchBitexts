35793adc3f5af7317ce2389c20d12d936ec194de
yaya-sy <yayasysco@gmail.com>
2023-01-03 13:41:07 +0100

    removing all grammatical descriptions

--- scripts/data_scrappers/data_scrappers/spiders/bamadaba.py
+++ scripts/data_scrappers/data_scrappers/spiders/bamadaba.py
@@ -16,12 +16,16 @@ class QuotesSpider(scrapy.Spider):
         """
         files = {"bambara": bambara_file, "french": french_file}
         languages = {"Exe" : "bambara", "GlFr" : "french"}
+        grammar_pattern = r"voyelles|voyelle|Verbes|personnels|Mots|infinitif|subjonctif|Construction"
         # p.lxP2 is for the word entry in the dictionnary
-        for entry in response.css("p.lxP2"):
+        for entry in response.css("p.lxP2,p.lxP"):
             last_tag = None
             sentences = {"bambara": [], "french": []}
             for span in entry.css("span"):
                 # we are only interested by the french and bambara sentences.
+                if "id" in span.attrib:
+                    word = span.css("*::text").get()
+                    id = span.attrib["id"]
                 if span.attrib["class"] not in ["Exe", "GlFr"]:
                     continue
                 # if the first example is french, it's probably just the translation
@@ -30,6 +34,8 @@ class QuotesSpider(scrapy.Spider):
                     continue
                 last_tag = span.attrib["class"]
                 for sentence in span.css(f"span.{last_tag} *::text").getall():
+                    if re.findall(grammar_pattern, sentence):
+                        continue
                     sentences[languages[last_tag]].append(sentence)
             # if we have don't have any examples
             if not sentences["bambara"] or not sentences["french"]:
@@ -46,7 +52,7 @@ class QuotesSpider(scrapy.Spider):
         """Get the translations in the audio examples."""
         sentences = {"bambara" : [], "french" : []}
         french_bambara_separator = r"\- \'|\- \‘|\- \«| ‘| \'| «"
-        grammar_pattern = r"voyelles|Verbes|personnels|Mots|infinitif|subjonctif|Construction"
+        grammar_pattern = r"voyelles|voyelle|Verbes|personnels|Mots|infinitif|subjonctif|Construction"
         for audio_example in response.css("div.maud small"):
             audio_capture = audio_example.css("*::text").getall()
             if not audio_capture:
@@ -62,24 +68,29 @@ class QuotesSpider(scrapy.Spider):
                 separated = re.split(french_bambara_separator, example)
                 if len(separated) != 2:
                     continue
-                sentences["bambara"].append(separated[0].strip())
-                sentences["french"].append(separated[1].strip())
+                bambara, french = separated[0].strip(), separated[1].strip()
+                if re.search(grammar_pattern, french) or re.search(grammar_pattern, bambara):
+                    continue
+                sentences["bambara"].append(bambara)
+                sentences["french"].append(french)
 
             elif len(audio_capture) == 2:
                 bambara, french = audio_capture
-                if re.search(grammar_pattern, french) or re.search(grammar_pattern, bambara):
-                    continue
                 if re.search(french_bambara_separator, bambara):
                     separated = re.split(french_bambara_separator, bambara)
                     if len(separated) != 2:
                         continue
                     bambara, french = separated
+                if re.search(grammar_pattern, french) or re.search(grammar_pattern, bambara):
+                    continue
                 sentences["bambara"].append(bambara.strip())
                 sentences["french"].append(french.strip())
             
             # the last element is just a grammatical description
             elif len(audio_capture) == 3:
                 bambara, french, _ = audio_capture
+                if re.findall(grammar_pattern, french) or re.findall(grammar_pattern, bambara):
+                    continue
                 sentences["bambara"].append(bambara.strip())
                 sentences["french"].append(french.strip())
             else:
